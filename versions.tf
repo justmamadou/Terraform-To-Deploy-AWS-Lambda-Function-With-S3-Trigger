d3191e32ab4bf1f5da7025af330b9b46aaa750c7
iam_mamadou <momodouba44@gmail.com>
2024-10-26 13:14:22 +0000

    Add files todeploy lambda

---
+++ versions.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = "1.6.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.32.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+# remote backend
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-backend-bucket-mamadou"
+    key            = "lambda-function/terraform.tfstate"
+    region         = "eu-west-1"
+    encrypt        = true
+  }
+}
